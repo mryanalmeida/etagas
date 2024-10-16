8c24c8b7a72d637910635cf240eae5a20f91b791
Marcos ryan <tubaraogalatico2509@gmail.com>
2024-10-16 20:34:49 -0300

    etagas

--- sw.js
+++ sw.js
@@ -3,3 +3,49 @@
  * @author Marcos Almeida
  * @link
  */
+
+//Instalar SW
+self.addEventListener('install', (event) => {
+    console.log("Sw Instalando", event)
+    //pre carregamento em cache
+    event.waitUntil(
+        caches.open('static')
+            .then((cache) => {
+                console.log("Pré carregamento do app")
+                cache.add('/')
+                cache.add('/index.html')
+                cache.add('/style.css')
+                cache.add('/app.js')
+                cache.add('/img/flex.png')
+                cache.add('/img/flex.png')
+                cache.add('/img/flex.png')
+                cache.add('/img/calcflex.png')
+                cache.add('/img/etanol.png')
+                cache.add('/img/gasolina.png')
+            })
+    )
+})
+
+
+//Ativação do Sw
+self.addEventListener('activate', (event) => {
+    console.log("Ativando Sw", event)
+    return self.clients.claim()
+})
+
+//escutando requisições "buscando algo "
+self.addEventListener('fetch', (event) => {
+    //console.log("Buscando Algo...", event)
+    //Armazenar em cache todas as requisiões
+    event.respondWith(
+        caches.match(event.request)
+            .then((resposne) => {
+                if (resposne) {
+                    return resposne
+                } else {
+                    return fetch(event.request)
+                }
+            })
+    )
+})
+
